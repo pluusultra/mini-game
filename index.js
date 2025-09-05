dbf46d36dd922d58740514552ebaa83690000052
pluusultra <ultracrackerrr@gmail.com>
2025-09-05 22:33:44 +0300

    рефактор и функция движения деревьев

--- index.js
+++ index.js
@@ -1,74 +1,36 @@
-const car = document.querySelector('.game__car');
-const gameWrapper = document.querySelector('.game-wrapper');
+const trees = document.querySelectorAll('.tree')
 
-let carShift = 0
-let carAcceleration = 6
-let carMoveStatus = {
-    moveRight: null,
-    moveLeft: null
-}
-const gameWrapperWidth = gameWrapper.clientWidth / 2
-const carWidth = car.clientWidth / 2
+const tree1 = trees[0];
+const speed = 2;
+let animationId = null
+let coordsTree1 = getCoords(tree1)
 
-document.addEventListener('keydown', (event) => {
-    if (event.code === 'KeyD' && carMoveStatus.moveRight === null) {
-        carMoveStatus.moveRight = requestAnimationFrame(moveCarToRight)
-    }
-    if (event.code === 'KeyA' && carMoveStatus.moveLeft === null) {
-        carMoveStatus.moveLeft = requestAnimationFrame(moveCarToLeft)
-    }
-})
+animationId = requestAnimationFrame(startGame)
 
-document.addEventListener('keyup', (event) => {
-    if (event.code === 'KeyD') {
-        cancelAnimationFrame(carMoveStatus.moveRight)
-        carMoveStatus.moveRight = null;
-        setDefaultRotate()
-    }
-    if (event.code === 'KeyA') {
-        cancelAnimationFrame(carMoveStatus.moveLeft)
-        carMoveStatus.moveLeft = null;
-        setDefaultRotate()
-    }
-})
-
-const moveCarToRight = () => {
-    const carCords = getCords(car)
-
-    if (carCords > gameWrapperWidth - carWidth) {
-        setDefaultRotate()
-        return
-    }
-
-    car.style.transform = `translateX(${carShift}px) rotate(15deg)`
-    carShift += carAcceleration
-    console.log('car cords: ', getCords(car))
-    console.log('wrapper cords: ', gameWrapperWidth - carWidth)
-    carMoveStatus.moveRight = requestAnimationFrame(moveCarToRight)
+function startGame() {
+    treesAnimation()
+    animationId = requestAnimationFrame(startGame)
 }
 
-const moveCarToLeft = () => {
-    const carCords = getCords(car)
-
-    if (carCords < -gameWrapperWidth + carWidth) {
-        setDefaultRotate()
-        return
+function getCoords(item) {
+    const matrix = window.getComputedStyle(item).transform;
+    const coords = matrix.split(',')
+    const y = coords[coords.length - 1]
+    const x = coords[coords.length - 2]
+    const numericY = parseFloat(y)
+    const numericX= parseFloat(x)
+    return {
+        y: numericY,
+        x: numericX
     }
-
-    car.style.transform = `translateX(${carShift}px) rotate(-15deg)`
-    carShift -= carAcceleration
-    console.log(getCords(car))
-    carMoveStatus.moveLeft = requestAnimationFrame(moveCarToLeft)
 }
 
-const setDefaultRotate = () => {
-    const currentCords = getCords(car)
-    car.style.transform = `translateX(${currentCords}px) rotate(0deg)`
+function treesAnimation () {
+    const newCoordY = coordsTree1.y + speed
+    console.log(newCoordY)
+    coordsTree1.y = newCoordY
+
+    tree1.style.transform = `translate(${coordsTree1.x}px, ${newCoordY}px)`
 }
 
-const getCords = (item) => {
-    const matrix = window.getComputedStyle(item).transform
-    const array = matrix.split(',')
-    const value = array[array.length - 2]
-    return parseFloat(value)
-}
+treesAnimation()
