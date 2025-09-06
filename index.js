2d89738c1b3bde94bb2839b6ba627a2ef9684a4f
pluusultra <ultracrackerrr@gmail.com>
2025-09-06 19:23:53 +0300

    реализовал функцию просчитывания коллизии

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-import {startMoveToLeft, startMoveToRight, moveStatus} from "./movePlayer";
-import {getCoords} from "./getCoords";
-import {treesAnimation, elementAnimation} from "./animation";
-
 const trees = document.querySelectorAll('.tree')
 const player = document.querySelector('.player')
 const road = document.querySelector('.road')
@@ -10,11 +6,24 @@ const coin = document.querySelector('.coin')
 const roadWidth = road.clientWidth / 2
 const playerWidth = player.clientWidth / 2
 const coinCoords = getCoords(coin)
+const playerCoords = getCoords(player)
+const playerHeight = player.clientHeight
 const coinWidth = coin.clientWidth / 2
 const coinHeight = coin.clientHeight
 let speed = 10;
+const treesCoords = []
 
 let animationId = null
+const moveStatus = {
+    moveRight: null,
+    moveLeft: null,
+}
+
+trees.forEach(tree => {
+    const coordsTree = getCoords(tree)
+
+    treesCoords.push(coordsTree)
+})
 
 document.addEventListener('keydown', (event) => {
     if (event.code === 'KeyD' || event.code === 'ArrowRight') {
@@ -42,19 +51,104 @@ document.addEventListener('keyup', (event) => {
     }
 })
 
-const treesCoords = []
+function checkCollision() {
+    const playerYTop = playerCoords.y
+    const playerYBottom = playerCoords.y + playerHeight;
 
-trees.forEach(tree => {
-    const coordsTree = getCoords(tree)
+    const playerXLeft = playerCoords.x - playerWidth;
+    const playerXRight = playerCoords.x + playerWidth;
 
-    treesCoords.push(coordsTree)
-})
+    const coinYTop = coinCoords.y;
+    const coinYBottom = coinCoords.y + coinHeight;
 
-animationId = requestAnimationFrame(startGame)
+    const coinXLeft = coinCoords.x - coinWidth;
+    const coinXRight = coinCoords.x + coinWidth
+
+    if (playerYTop > coinYBottom || playerYBottom < coinYTop) {
+        return false
+    }
+
+    if (playerXLeft > coinXRight || playerXRight < coinXLeft) {
+        return false
+    }
+
+    return true
+}
+
+function startMoveToRight () {
+    if (playerCoords.x > roadWidth - playerWidth) {
+        return
+    }
+
+    player.style.transform = `translate(${playerCoords.x + 5}px, ${playerCoords.y}px)`
+    playerCoords.x += 5
+    moveStatus.moveRight = requestAnimationFrame(startMoveToRight)
+}
+
+function startMoveToLeft () {
+
+
+    if (playerCoords.x < -roadWidth + playerWidth) {
+        return
+    }
+    player.style.transform = `translate(${playerCoords.x - 5}px, ${playerCoords.y}px) scaleX(-1)`
+    playerCoords.x -= 5;
+    moveStatus.moveLeft = requestAnimationFrame(startMoveToLeft)
+}
 
 function startGame() {
+    console.log(checkCollision())
     treesAnimation()
     elementAnimation(coin, coinCoords, coinWidth, 1000)
     animationId = requestAnimationFrame(startGame)
 }
+
+
+function getCoords(item) {
+    const matrix = window.getComputedStyle(item).transform;
+    const coords = matrix.split(',')
+    const y = coords[coords.length - 1]
+    const x = coords[coords.length - 2]
+    const numericY = parseFloat(y)
+    const numericX= parseFloat(x)
+    return {
+        y: numericY,
+        x: numericX
+    }
+}
+
+function elementAnimation(element, elementCoords, elementWidth, innitialCoordY) {
+    let newCoordY = elementCoords.y + speed;
+    let newCoordX = elementCoords.x;
+
+    if (newCoordY > window.innerHeight) {
+        const direction = Math.floor(Math.random() * 2)
+        const maxXCord = (roadWidth + 1 - elementWidth)
+        const randomXCoord = Math.floor(Math.random() * (roadWidth + 1));
+        newCoordX = direction === 0 ? -randomXCoord : randomXCoord;
+        newCoordY = -innitialCoordY
+    }
+
+    elementCoords.y = newCoordY;
+    elementCoords.x = newCoordX
+    element.style.transform = `translate(${newCoordX}px, ${newCoordY}px)`
+}
+
+function treesAnimation () {
+    for (let i = 0; i < trees.length; i++) {
+        const tree = trees[i];
+        const coords = treesCoords[i]
+        let newCoordY = coords.y + speed
+
+        if (newCoordY > window.innerHeight) {
+            newCoordY = -tree.height
+        }
+
+        treesCoords[i].y = newCoordY;
+
+        tree.style.transform = `translate(${coords.x}px,${newCoordY}px)`
+    }
+}
+
+animationId = requestAnimationFrame(startGame)
 
